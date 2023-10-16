7cfd4a11d954bbe68c355148fad9af7ee14caa0b
Your Name <you@example.com>
2023-10-16 19:49:22 +0500

    latest most most commit

--- src/Components/Products/TrendingProducts/TrendingProducts.tsx
+++ src/Components/Products/TrendingProducts/TrendingProducts.tsx
@@ -1,4 +1,3 @@
-
 import './TremdingProducts.css';
 import { Link } from 'react-router-dom';
 
@@ -7,14 +6,14 @@ const TrendingProducts = () => {
         <div className="trending-products">
             <div className="trending-background-image" > </div>
             <div className="trending-content">
-               <p className='trending-heading'>Trending</p>
-               <p className='trending-paragraph'>Show off your personality with meaningful, eco-conscious expandable handmade bag's, that empower the light within you.</p>
-               <Link to="/pages/collections">
-                SHOP NOW
-               </Link>
+                <p className='trending-heading'>Trending</p>
+                <p className='trending-paragraph'>Show off your personality with meaningful, eco-conscious expandable handmade bag's, that empower the light within you.</p>
+                <Link to="/pages/collections">
+                    SHOP NOW
+                </Link>
             </div>
         </div>
     );
 }
 
-export default TrendingProducts
+export default TrendingProducts;
